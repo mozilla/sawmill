d146d93644ebc8a294d477c1f21485916c852046
Christopher De Cairos <cade@mozillafoundation.org>
2014-04-11 10:35:11 -0400

    Add option for disabling sending event staff emails

--- worker/send_mofo_staff_email.js
+++ worker/send_mofo_staff_email.js
@@ -1,7 +1,7 @@
 module.exports = function(notifier_messager) {
   var NOTIFIER_EVENT_TYPE = "send_mofo_staff_email";
   return function(id, event, cb) {
-    if (event.event_type === "create_event") {
+    if (event.event_type === "create_event" && !(event.data.sendMofoStaffEmail === false)) {
       notifier_messager.sendMessage({
         event_type: NOTIFIER_EVENT_TYPE,
         data: {
