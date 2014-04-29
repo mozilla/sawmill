915c267e0088bd62ac3a9ea627d6331c84a8aa63
Kate Hudson <k88hudson@gmail.com>
2014-04-29 11:50:13 -0700

    Bug 1000472 - added emails for issued badges

---
+++ worker/badge_awarded_send_email.js
@@ -0,0 +1,16 @@
+module.exports = function(notifier_messager) {
+  var NOTIFIER_EVENT_TYPE = "badge_awarded_send_email";
+  return function(id, event, cb) {
+    if (event.event_type === "badge_awarded" && !(event.data.sendEmail === false)) {
+      notifier_messager.sendMessage({
+        event_type: NOTIFIER_EVENT_TYPE,
+        data: {
+          email: event.data.email,
+          badge: event.data.badge
+        }
+      }, cb);
+    } else {
+      process.nextTick(cb);
+    }
+  };
+};
--- worker/index.js
+++ worker/index.js
@@ -6,5 +6,6 @@ module.exports = {
   send_event_host_email: require("./send_event_host_email"),
   send_mofo_staff_email: require("./send_mofo_staff_email"),
   send_new_user_email: require("./send_new_user_email"),
-  sign_up_for_bsd: require("./sign_up_for_bsd")
+  sign_up_for_bsd: require("./sign_up_for_bsd"),
+  badge_awarded_send_email: require("./badge_awarded_send_email")
 };
