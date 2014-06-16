85fddc3b1b2e169e4c620bb772ac1339ec7f403a
Christopher De Cairos <cade@mozillafoundation.org>
2014-06-16 10:43:35 -0400

    login token worker

--- processor.js
+++ processor.js
@@ -30,6 +30,7 @@ var workers = async.applyEachSeries([
 //  worker.user_stats(redis_config),
 //  worker.referrer_stats(redis_config),
   worker.remind_user_about_event(notifier_messager, mailroom),
+  worker.send_one_time_password(notifier_messager, mailroom),
   worker.send_event_host_email(notifier_messager, mailroom),
   worker.send_mofo_staff_email(notifier_messager, mailroom),
   worker.send_new_user_email(notifier_messager, mailroom),
---
+++ worker/send_one_time_password.js
@@ -0,0 +1,27 @@
+module.exports = function(notifier_messager, mailroom) {
+  var LUMBERYARD_EVENT = 'mailer';
+  var FROM_EMAIL = 'Webmaker <help@webmaker.org>';
+
+  return function(id, event, cb) {
+
+    if (event.event_type !== "login_token_email") {
+      return process.nextTick(cb);
+    }
+
+    var mail = mailroom.render('login_request', {
+      username: event.data.username,
+      token: event.data.token,
+      loginUrl: event.data.loginUrl
+    }, event.data.locale);
+
+    notifier_messager.sendMessage({
+      event_type: LUMBERYARD_EVENT,
+      data: {
+        from: FROM_EMAIL,
+        to: event.data.email,
+        subject: mail.subject,
+        html: mail.html
+      }
+    }, cb);
+  };
+};
