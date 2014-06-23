58ae42f4a510f463d257e26af750360dcc769d24
Kate Hudson <k88hudson@gmail.com>
2014-06-23 15:04:46 -0400

    Bug 1011135 - Added hive_badge_awarded worker

---
+++ worker/hive_badge_awarded.js
@@ -0,0 +1,39 @@
+module.exports = function(lumberyard_messager) {
+  var SAWMILL_EVENT = 'hive_badge_awarded';
+  var LUMBERYARD_EVENT = 'mailer';
+  var EVENTS_FROM_EMAIL = 'Webmaker <help@webmaker.org>';
+  var mailroom = require('webmaker-mailroom')();
+
+  return function(id, event, cb) {
+    var sendEmail = event.data.sendEmail;
+    var username = event.data.username;
+    var email = event.data.email;
+    var locale = event.data.locale;
+    var badgeUrl = event.data.badgeUrl;
+    var profileUrl = event.data.profileUrl;
+    var signUpUrl = event.data.signUpUrl;
+    var comment = event.data.comment;
+
+    var mail = mailroom.render(SAWMILL_EVENT, {
+      username: username,
+      badgeUrl: badgeUrl,
+      profileUrl:  profileUrl,
+      signUpUrl: signUpUrl,
+      comment: comment
+    }, locale);
+
+    if (event.event_type === SAWMILL_EVENT && !(sendEmail === false)) {
+      lumberyard_messager.sendMessage({
+        event_type: LUMBERYARD_EVENT,
+        data: {
+          from: EVENTS_FROM_EMAIL,
+          to: email,
+          subject: mail.subject,
+          html: mail.html
+        }
+      }, cb);
+    } else {
+      process.nextTick(cb);
+    }
+  };
+};
