c7ead01c8c3c3fb93959e3848c98ccc64df58b17
Kate Hudson <k88hudson@gmail.com>
2014-06-24 16:12:50 -0400

    f

--- worker/event_coorganizer_added.js
+++ worker/event_coorganizer_added.js
@@ -1,16 +1,16 @@
 module.exports = function(lumberyard_messager, mailroom) {
   var SAWMILL_EVENT = 'event_coorganizer_added';
   var LUMBERYARD_EVENT = 'mailer';
-  var EVENTS_FROM_EMAIL = 'Webmaker <help@webmaker.org>';
+  var FROM_EMAIL = 'Webmaker <help@webmaker.org>';
 
   return function(id, event, cb) {
     var sendEmail = event.data.sendEmail;
     var email = event.data.email;
     var locale = event.data.locale;
     var data = {
-      username = event.data.username,
-      eventName = event.data.eventName,
-      eventUrl = event.data.eventUrl,
+      username: event.data.username,
+      eventName: event.data.eventName,
+      eventUrl: event.data.eventUrl,
       eventEditUrl: event.data.eventEditUrl
     };
 
@@ -20,7 +20,7 @@ module.exports = function(lumberyard_messager, mailroom) {
       lumberyard_messager.sendMessage({
         event_type: LUMBERYARD_EVENT,
         data: {
-          from: EVENTS_FROM_EMAIL,
+          from: FROM_EMAIL,
           to: email,
           subject: mail.subject,
           html: mail.html
