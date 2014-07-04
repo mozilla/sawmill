6404788b215e5d573da2c5646e5584e3fadcadff
Kate Hudson <k88hudson@gmail.com>
2014-07-04 13:16:48 -0400

    Bug 1030885 - Use new mailroom for event creation emails

--- worker/send_event_host_email.js
+++ worker/send_event_host_email.js
@@ -1,13 +1,21 @@
-module.exports = function(notifier_messager) {
-  var NOTIFIER_EVENT_TYPE = "send_event_host_email";
+module.exports = function(notifier_messager, mailroom) {
+  var LUMBERYARD_EVENT = "mailer";
+  var FROM_EMAIL = 'Webmaker <help@webmaker.org>';
+
   return function(id, event, cb) {
     if (event.event_type === "create_event" && event.data.sendEventCreationEmails) {
+
+      var mail = mailroom.render("event_created", {
+        username: event.data.username
+      }, event.data.locale);
+
       notifier_messager.sendMessage({
-        event_type: NOTIFIER_EVENT_TYPE,
+        event_type: LUMBERYARD_EVENT,
         data: {
-          username: event.data.username,
-          email: event.data.email,
-          locale: event.data.locale
+          from: FROM_EMAIL,
+          to: event.data.email,
+          subject: mail.subject,
+          html: mail.html
         }
       }, cb);
     } else {
