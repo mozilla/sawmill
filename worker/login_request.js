e5223bd9d0a462080762c327d70061ec07e285a9
Christopher De Cairos <chris@chrisdecairos.ca>
2015-04-08 10:05:27 -0400

    Add Migration email logic

--- worker/login_request.js
+++ worker/login_request.js
@@ -8,13 +8,21 @@ module.exports = function(notifier_messager, mailroom) {
       return process.nextTick(cb);
     }
 
-    var templateName = event.data.verified === true ? 'login_request' : 'account_confirmation';
-
-    var mail = mailroom.render(templateName, {
+    var templateName;
+    var mailOptions = {
       username: event.data.username,
-      token: event.data.token,
-      loginUrl: event.data.loginUrl
-    }, event.data.locale);
+      token: event.data.token
+    };
+
+    if ( event.data.migrateUser ) {
+      templateName = 'migrate_user';
+      mailOptions.migrateUrl = event.data.migrateUrl;
+    } else {
+      templateName = event.data.verified === true ? 'login_request' : 'account_confirmation';
+      mailOptions.loginUrl = event.data.loginUrl;
+    }
+
+    mail = mailroom.render(templateName, mailOptions, event.data.locale);
 
     notifier_messager.sendMessage({
       event_type: LUMBERYARD_EVENT,
