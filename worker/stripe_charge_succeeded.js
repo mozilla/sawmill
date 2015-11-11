613eb2fbec94d706a6d971c42d5ddc00e1e83d61
Jon Buckley <jon@jbuckley.ca>
2015-11-11 15:28:30 -0500

    Implement non-en-US email for stripe_charge_succeeded

--- worker/stripe_charge_succeeded.js
+++ worker/stripe_charge_succeeded.js
@@ -1,5 +1,3 @@
-var getCurrencySymbol = require('currency-symbol-map')
-
 // https://support.stripe.com/questions/which-zero-decimal-currencies-does-stripe-support
 var zeroDecimalCurrencies = [
   'BIF',
@@ -19,39 +17,53 @@ var zeroDecimalCurrencies = [
   'XPF'
 ];
 
+var localesWith2015Email = [
+  'en-US'
+];
+
 module.exports = function(notifier_messager, mailroom) {
   const LUMBERYARD_EVENT = 'mailer';
   const FROM_EMAIL = 'The Mozilla Team <donate@mozilla.org>';
 
   return function(event, cb) {
 
-    if (event.event_type !== "stripe_charge_succeeded") {
+    if (event.event_type !== 'stripe_charge_succeeded') {
       return process.nextTick(cb);
     }
 
     var currency_code = event.data.currency.toUpperCase();
+    var locale = event.data.metadata.locale;
+
     // If this is a zero decimal currency then use it directly
     // Otherwise divide by 100 to get currency major.minor amount
-    var amount;
-    if (zeroDecimalCurrencies.indexOf(currency_code) > -1) {
-      amount = event.data.amount;
+    var amount = event.data.amount;
+    if (zeroDecimalCurrencies.indexOf(currency_code) === -1) {
+      amount = amount / 100;
+    }
+    amount = new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: currency_code
+    }).format(amount);
+
+    var template_name = 'stripe_charge_succeeded';
+    if (localesWith2015Email.indexOf(locale) > -1) {
+      template_name = template_name + '_2015';
     } else {
-      amount = (event.data.amount / 100).toFixed(2);
+      template_name = template_name + '_2014';
     }
 
-    var email = mailroom.render('stripe_charge_succeeded', {
+    var email = mailroom.render(template_name, {
       name: event.data.source.name,
       amount: amount,
       // If there's an invoice attached to this charge, then it's a subscription
+      // The 2014 variant doesn't display anything different with this parameter
       recurring_donation: !!event.data.invoice,
-      currency_symbol: getCurrencySymbol(currency_code),
-      currency_code: currency_code,
       address: event.data.source.address_line1,
       city: event.data.source.address_city,
       state: event.data.source.state,
       zipcode: event.data.source.address_zip,
       country: event.data.source.address_country
-    }, event.data.metadata.locale);
+    }, locale);
 
     notifier_messager.sendMessage({
       event_type: LUMBERYARD_EVENT,
